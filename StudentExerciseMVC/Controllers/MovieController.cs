0d0457024dd6e660a2ee1591b0256d2fadf0822c
LowDown322 <johannes.pettersson@edunord.se>
2024-01-18 12:53:37 +0100

    first task ASP.NET

---
+++ StudentExerciseMVC/Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentExerciseMVC.Models;
+
+namespace StudentExerciseMVC.Controllers
+{
+  public class MovieController : Controller
+  {
+    private static List<MovieModelClass> _movies = new List<MovieModelClass>();
+    public IActionResult Index1()
+    {
+     // MovieModelClass movie = new MovieModelClass();
+    //  { movie.MovieTitle = "the godfather"; movie.Director = "Francis Ford Coppola"; movie.Year = 1972; }
+      return View(_movies);
+    }
+    public IActionResult Index2()
+    {
+      var movieVm = new MovieModelClass();
+      return View(movieVm);
+    }
+    public IActionResult CreateMovie(MovieModelClass movieViewModel) 
+    {
+
+      _movies.Add(movieViewModel);
+      return RedirectToAction(nameof(Index));
+      // return View("")
+    }
+  }
+}
---
+++ StudentExerciseMVC/Models/MovieModelClass.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentExerciseMVC.Models
+{
+  public class MovieModelClass
+  {
+    public string MovieTitle { get; set; }
+    public string Director { get; set;}
+
+    public int Year { get; set;}
+  }
+}
